1dbbe134adf576c0617b6017715b22174a4b5581
Conrad Irwin <conrad.irwin@gmail.com>
2012-06-16 14:46:45 -0700

    Make label and button visible

--- app/controllers/my_view_controller.rb
+++ app/controllers/my_view_controller.rb
@@ -2,12 +2,18 @@ class MyViewController < UIViewController
   attr_accessor :button
 
   layout do
-    subview UILabel,
-      text: "Hello World"
+    subview(UILabel,
+      text: "Hello World",
+      top: 60, left: 60,
+      width: 200, height: 100,
+      backgroundColor: UIColor.blueColor
+    )
 
     self.button = subview(
       UIButton.buttonWithType(UIButtonTypeCustom),
-      title: "Click me!"
+      title: "Click me!",
+      top: 200, left: 60,
+      width: 200, height: 100
     )
   end
 end
